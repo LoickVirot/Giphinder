6e2878b55231436bc42b5b9dd4076664e97b4e0f
Eloteck <eloteck@outlook.fr>
2017-07-15 19:02:26 +0200

    create config file for api keys

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,14 @@
  * Created by Loïck on 15/07/2017.
  */
 import axios from 'axios';
+import {GIPHY} from '../config/api_keys';
 
 export const FETCH_GIFS = 'FETCH_GIFS';
 export const ACTIVE_SEARCH = 'ACTIVE_SEARCH';
 export const SELECT_GIF = 'SELECT_GIF';
 export const CLOSE_GIF_MODAL = 'CLOSE_GIF_MODAL';
 
-const API_KEY = '5ae052aa25754b00abb6811fb76d85f8';
+const API_KEY = GIPHY;
 
 export function fetchGifs(term) {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${term}&limit=25&offset=0&rating=G`;
---
+++ src/config/api_keys.js
@@ -0,0 +1,1 @@
+export const GIPHY = '5ae052aa25754b00abb6811fb76d85f8';
